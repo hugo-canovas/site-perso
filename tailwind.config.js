adf40c3e9cf78eb0e4d3f76052a2153d35d79e51
hugocanovas <canovashugo83@gmail.com>
2023-06-30 15:15:48 +0200

    Ajout flowbite + refonte du css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: [
+    'class',
+  ],
   content: [
     "./index.html",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
